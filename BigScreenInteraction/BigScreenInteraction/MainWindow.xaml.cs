c30d3bf2c21f292ccb8d90d179b19db04923245b
huiya9527 <huiya9527@163.com>
2016-06-07 20:26:02 +0800

    主窗口布局修改
    增加了项目启动按钮，背景设置透明，窗口大小不可调节等必要功能

--- BigScreenInteraction/BigScreenInteraction/MainWindow.xaml.cs
+++ BigScreenInteraction/BigScreenInteraction/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Configuration;
+using System.Diagnostics;
 
 namespace BigScreenInteraction
 {
@@ -20,6 +10,8 @@ namespace BigScreenInteraction
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -30,5 +22,69 @@ namespace BigScreenInteraction
             MouseControlConfiguration mouseConfig = new MouseControlConfiguration();
             mouseConfig.Show();
         }
+
+
+        //启动手势识别界面
+        private void GestureButtonClick(object sender, RoutedEventArgs e)
+        {
+            ButtonGird.Visibility = Visibility.Collapsed;
+            //GestrureDisplayGrid.Visibility = Visibility.Visible;
+            //GestureImage.Source = null;
+        }
+
+        //启动姿势识别界面 
+        private void PostureButtonClick(object sender, RoutedEventArgs e)
+        {
+            ButtonGird.Visibility = Visibility.Collapsed;
+            //PostureDisplayGrid.Visibility = Visibility.Visible;
+            //PostureImage.Source = null;
+        }
+
+        //离开按钮
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(0);
+        }
+
+        //用于设置程序始终在最上层
+        public void setFullScreen(object sender, EventArgs e)
+        {
+            Window window = (Window)sender;
+            window.Topmost = true;
+        }
+        //启动用户3D地图,这里用画图程序替代，但是不好操作！如何退出？
+        private void Map_3D(object sender, RoutedEventArgs e)
+        {
+            ButtonGird.Visibility = Visibility.Collapsed;
+            try
+            {
+                //_WBFramework._GRRecognizer.TUIO_MOUSE = true;
+                //var drawing = ConfigurationManager.AppSettings["drawing"];
+                var drawing = ConfigurationManager.AppSettings["drawing"];
+                ProcessHandler.process1 = Process.Start(drawing);
+
+            }
+            catch { }
+        }
+        //启动用户2D地图，这里用记事本程序替代，可能也不太好操作！如何退出？
+        private void Map_2D(object sender, RoutedEventArgs e)
+        {
+            ButtonGird.Visibility = Visibility.Collapsed;
+            try
+            {
+
+                //_WBFramework._GRRecognizer.TUIO_MOUSE = true;
+                var notepad = ConfigurationManager.AppSettings["notepad"];
+                ProcessHandler.process2 = Process.Start(notepad);
+
+            }
+            catch { }
+        }
+    }
+
+    public static class ProcessHandler
+    {
+        public static Process process1;
+        public static Process process2;
     }
 }
